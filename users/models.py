fda69ab89e43a4103cae4178427e5904d2e45d41
Nikita Solouyou <solovev.nikita.05@gmail.com>
2023-09-22 12:45:36 +0300

    Create UserLessonAccess model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+from lessons.models import Lesson
+
+User = get_user_model()
+
+
+class UserLessonAccess(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+    view_time_seconds = models.PositiveIntegerField(default=0)
+
+    @property
+    def viewed(self) -> bool:
+        return self.view_time_seconds >= 0.8 * self.lesson.duration_seconds

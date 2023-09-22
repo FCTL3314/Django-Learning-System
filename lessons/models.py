c5f5b02081c45c0c371c236fdeee09e568370609
Nikita Solouyou <solovev.nikita.05@gmail.com>
2023-09-22 12:45:23 +0300

    Create lesson model

--- lessons/models.py
+++ lessons/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from products.models import Product
+
+
+class Lesson(models.Model):
+    name = models.CharField(max_length=255)
+    video_link = models.URLField()
+    duration_seconds = models.PositiveIntegerField()
+    products = models.ManyToManyField(Product, related_name="lessons")
+
+    def __str__(self):
+        return self.name
